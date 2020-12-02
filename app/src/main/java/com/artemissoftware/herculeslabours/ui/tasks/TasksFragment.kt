dc5d5e8f6edfc60efae3a1b4aedcb468bd4ebfc6
gustavo <gustavo.maia@b2b.com.pt>
2020-12-02 10:44:13 +0000

    ListAdapter with View Binding

--- app/src/main/java/com/artemissoftware/herculeslabours/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/artemissoftware/herculeslabours/ui/tasks/TasksFragment.kt
@@ -1,8 +1,13 @@
 package com.artemissoftware.herculeslabours.ui.tasks
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.artemissoftware.herculeslabours.R
+import com.artemissoftware.herculeslabours.databinding.FragmentTasksBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,4 +15,25 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
     private val viewModel : TasksViewModel by viewModels()
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTasksBinding.bind(view)
+
+        val taskAdapter = TasksAdapter()
+
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter = taskAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+
+
+        viewModel.tasks.observe(viewLifecycleOwner) {
+            taskAdapter.submitList(it)
+        }
+    }
 }
--- app/src/main/java/com/artemissoftware/herculeslabours/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/artemissoftware/herculeslabours/ui/tasks/TasksViewModel.kt
@@ -1,8 +1,13 @@
 package com.artemissoftware.herculeslabours.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.artemissoftware.herculeslabours.data.TaskDao
 
 class TasksViewModel @ViewModelInject constructor(private val taskDao: TaskDao) : ViewModel(){
+
+
+    val tasks = taskDao.getTasks().asLiveData()
 }
