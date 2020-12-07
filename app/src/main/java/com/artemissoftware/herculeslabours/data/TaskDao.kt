acd43164185abcc714817ff0efe6d0cdf704792f
gustavo <gustavo.maia@b2b.com.pt>
2020-12-07 11:33:47 +0000

    Combining multiple Flows

--- app/src/main/java/com/artemissoftware/herculeslabours/data/TaskDao.kt
+++ app/src/main/java/com/artemissoftware/herculeslabours/data/TaskDao.kt
@@ -1,6 +1,7 @@
 package com.artemissoftware.herculeslabours.data
 
 import androidx.room.*
+import com.artemissoftware.herculeslabours.ui.tasks.SortOrder
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -8,6 +9,21 @@ interface TaskDao {
 
     @Query("SELECT * FROM task_table WHERE name LIKE '%' || :searchQuery || '%' ORDER BY important DESC ")
     fun getTasks(searchQuery: String): Flow<List<Task>>
+
+
+    fun getTasks(searchQuery: String, sortOrder: SortOrder, hideCompleted: Boolean): Flow<List<Task>> =
+        when (sortOrder){
+            SortOrder.BY_DATE -> getTasksSortedByDateCreated(searchQuery, hideCompleted)
+            SortOrder.BY_NAME -> getTasksSortedByName(searchQuery, hideCompleted)
+        }
+
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, name ")
+    fun getTasksSortedByName(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
+
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, created ")
+    fun getTasksSortedByDateCreated(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
+
+
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(task: Task)
